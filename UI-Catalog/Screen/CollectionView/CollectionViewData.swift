0e0b183d7da6ffe9edfb05e00a9a4bbb4def3a7a
i.kawashima41 <i.kawashima41@gmail.com>
2019-05-15 03:15:17 +0900

    Data保持 struct

---
+++ UI-Catalog/Screen/CollectionView/CollectionViewData.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewData.swift
+//  UI-Catalog
+//
+//  Created by Ochi Kanta on 2019/05/15.
+//  Copyright © 2019 Iichiro Kawashima. All rights reserved.
+//
+
+import UIKit
+
+struct CollectionViewData {
+    static var sectionNumber: Int!
+}
