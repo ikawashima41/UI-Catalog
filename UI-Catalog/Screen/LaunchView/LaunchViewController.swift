b364b5c929ec6f7308f98b73468d61f15f7c4685
i.kawashima41 <i.kawashima41@gmail.com>
2019-05-19 20:36:24 +0900

    LaunchAnimationを実装

--- UI-Catalog/Screen/LaunchView/LaunchViewController.swift
+++ UI-Catalog/Screen/LaunchView/LaunchViewController.swift
@@ -7,15 +7,52 @@
 //
 
 import UIKit
+import Lottie
 
 class LaunchViewController: UIViewController {
 
+    struct Const {
+        static let waveAnimation = "wave_animation"
+        static let dinoAnimation = "dino_loading_animation"
+    }
+
+    @IBOutlet weak var animationView: AnimationView! {
+        didSet {
+            /**
+             MARK: - Use of undeclared type 'LOTAnimationView' · Issue #847 · airbnb/lottie-ios
+             https://github.com/airbnb/lottie-ios/issues/847
+             AnimationView causes crash when initialized in Interface Builder · Issue #826 · airbnb/lottie-ios
+             https://github.com/airbnb/lottie-ios/issues/826
+             */
+            let animation = Animation.named(Const.waveAnimation)
+            animationView.animation = animation
+            animationView.contentMode = .scaleAspectFill
+            animationView.backgroundColor = .clear
+            animationView.animationSpeed = 1
+        }
+    }
+
+    @IBOutlet weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.textColor = .white
+            titleLabel.font = UIFont.systemFont(ofSize: 28)
+            titleLabel.textAlignment = .center
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
+
+    override func viewDidAppear(_ animated: Bool) {
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: MainViewController.className)
+        let nc = UINavigationController(rootViewController: vc)
+
+        animationView.play { [weak self] _ in
+            self?.animationView.removeFromSuperview()
+            self?.present(nc, animated: false)
+        }
+    }
 
     /*
     // MARK: - Navigation
