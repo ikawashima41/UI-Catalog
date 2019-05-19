eac81b89c6209dfbd92c877fb315310f863beb72
i.kawashima41 <i.kawashima41@gmail.com>
2019-05-19 19:27:27 +0900

    アプリの起動時の画面を変更。 MainVC => LaunchVC

--- UI-Catalog/AppDelegate.swift
+++ UI-Catalog/AppDelegate.swift
@@ -13,14 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: MainViewController.className)
-        let nc = UINavigationController(rootViewController: vc)
+        let vc = UIStoryboard(name: LaunchViewController.className, bundle: nil).instantiateViewController(withIdentifier: LaunchViewController.className)
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = nc
+        window?.rootViewController = vc
         window?.makeKeyAndVisible()
 
         return true
