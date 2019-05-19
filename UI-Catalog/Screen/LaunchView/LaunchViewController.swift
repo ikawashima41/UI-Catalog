9523257323b6e5a45f65af52d07ec35ab4cbd826
i.kawashima41 <i.kawashima41@gmail.com>
2019-05-19 16:05:58 +0900

    LaunchViewController.swift+storyboardを追加

---
+++ UI-Catalog/Screen/LaunchView/LaunchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchViewController.swift
+//  UI-Catalog
+//
+//  Created by member on 2019/05/19.
+//  Copyright © 2019年 Iichiro Kawashima. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
