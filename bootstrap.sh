31a21599f39c9a9f49d4c74dac69acb76d72dd45
i.kawashima41 <i.kawashima41@gmail.com>
2019-06-30 17:58:55 +0900

    起動スクリプト修正

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Script to build iOS-UI-Catalog project
 
+# setup Gems, Brews and Mints
+echo "Install Gems and Brews"
+bundle install
+brew bundle
+
 # install dependencies via Carthage
 echo "Install dependencies via Carthage"
 carthage bootstrap --no-use-binaries --cache-builds --platform ios
